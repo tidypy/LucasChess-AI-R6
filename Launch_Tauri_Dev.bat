ad00eb30cc8ebb7d4a2f8ea521dd4e49e7585e66
tidypy <50213641+tidypy@users.noreply.github.com>
2026-08-21 22:33:08 -0500

    chore: remove redundant Launch_Tauri_Dev.bat script in favor of unified Launch_App.bat

--- Launch_Tauri_Dev.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-set "PATH=C:\Users\Dev\.cargo\bin;C:\Program Files\nodejs;%PATH%"
-echo ========================================
-echo Launching DeepScout Chess (Tauri + React)
-echo ========================================
-npm run tauri dev
-pause
